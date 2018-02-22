9ed323b5367fa1e3c93a955d2a4d1f27edb104f9
leedoughty <leedoughtydesign@gmail.com>
2018-02-22 12:29:15 +0000

    adding a second test

---
+++ InteractiveInfiniteSpin/empty-example/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+    createCanvas(1500, 800, WEBGL);
+    background(255);
+}
+
+function draw() {
+    noFill(255);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+    box(200, 200, 200, mouseX, mouseY);
+
+    fill(250, 200, 200);
+    noStroke();
+    ellipse(mouseX, mouseY, 50, 50);
+
+    fill(0, 255, 255);
+    noStroke();
+    triangle(mouseX, mouseY, 50, 20, 86, 75);
+}
+
+function mousePressed() {
+    background(250, 250, 100);
+}
